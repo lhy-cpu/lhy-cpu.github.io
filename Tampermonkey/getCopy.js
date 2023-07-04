3fa895dd8b51cefb4a029b241ce07ac8404fcf48
lhy-cpu <60498844+lhy-cpu@users.noreply.github.com>
2023-07-04 21:57:22 +0800

    Update getCopy.js
    Fix some bugs and add some extact function.

--- Tampermonkey/getCopy.js
+++ Tampermonkey/getCopy.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         getCopyEasyScript
 // @namespace    https://lhy-cpu.github.io
-// @version      1.1
+// @version      1.2
 // @description  Make it easy to copy something and remove annoying login windows.
 // @author       lhy-cpu
 // @match        *://*/*
@@ -41,6 +41,8 @@
             doc[acou].style = tmp;
         };
     });
-    setInterval('var eee = document.querySelectorAll("span");for (var i = 0; i < eee.length; i++) {if (eee[i].innerText == "×" && eee[i].parentNode.className=="passport-login-box") {eee[i].click();}};', 10);
-    setInterval('var classArr = [".ZDI.ZDI--Xmark16.css-1rgloxd",".Modal-closeButton"];for (var i = 0; i < classArr.length; i++) {var aaa = document.querySelector(classArr[i]);if(aaa!=null){aaa.click()};};', 10);
+    setInterval('', 10);
+    setInterval('var classArr = [".Modal-closeButton"];for (var i = 0; i < classArr.length; i++) {var aaa = document.querySelector(classArr[i]);if(aaa!=null){aaa.click()};};' +
+     'var eleArr = [".css-1ynzxqw"];for(var i = 0; i < eleArr.length; i++){var tt = document.querySelector(eleArr[i]);tt.innerHTML = "";};' +
+     'var eee = document.querySelectorAll("span");for (var i = 0; i < eee.length; i++) {if (eee[i].innerText == "×" && eee[i].parentNode.className=="passport-login-box") {eee[i].click();}};', 10);
 })();
